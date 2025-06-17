2c55cec7fff5bf1142494f9a6cf46d83c8276e96
KhurandaaboldTuguldur <hitugii.tb@gmail.com>
2025-06-17 09:27:39 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,20 +1,42 @@
-$(document).ready(function(){"use strict";document.getElementById("copyrightYear").innerHTML=(new Date).getFullYear(),$(".widget-slider").slick({dots:!1,infinite:!0,speed:300,slidesToShow:1,slidesToScroll:1,arrows:!0,autoplay:!0,responsive:[{breakpoint:992,settings:{slidesToShow:1,slidesToScroll:1}},{breakpoint:768,settings:{slidesToShow:1,slidesToScroll:1}}]}),$(window).on("scroll",function(){$(window).scrollTop()?$("nav").addClass("nav-bg"):$("nav").removeClass("nav-bg")})});
-fetch('/posts/index.json') // JSON файлд бүх markdown жагсаалт байна
-  .then(res => res.json())
-  .then(posts => {
-    const articleList = document.getElementById('articles-list');
-    const trendingList = document.getElementById('trending-posts');
-    posts.forEach((post, i) => {
-      const html = `
-        <div class="col-lg-4 blog-post">
-          <a href="single-blog.html?slug=${post.slug}">
-            <img src="${post.image}" alt="">
-            <h4>${post.title}</h4>
-            <p>${post.excerpt}</p>
-          </a>
-        </div>
-      `;
-      articleList.innerHTML += html;
-      if (i < 3) trendingList.innerHTML += html;
+fetch('/posts')
+  .then(response => response.text())
+  .then(text => {
+    const parser = new DOMParser();
+    const html = parser.parseFromString(text, 'text/html');
+    const links = [...html.querySelectorAll('a')];
+    const markdownFiles = links.filter(link => link.href.endsWith('.md'));
+
+    markdownFiles.forEach((file, index) => {
+      fetch(file.href)
+        .then(res => res.text())
+        .then(content => {
+          const title = content.match(/title:\s*(.*)/)[1];
+          const date = content.match(/date:\s*(.*)/)[1];
+          const desc = content.match(/description:\s*(.*)/)[1];
+          const thumb = content.match(/thumbnail:\s*(.*)/)[1];
+
+          const html = `
+            <div class="col-lg-4">
+              <article class="blog-post">
+                <div class="blog-post-thumb">
+                  <img src="${thumb}" alt="thumb" />
+                </div>
+                <div class="blog-post-content">
+                  <a href="single-blog.html" class="blog-post-title">${title}</a>
+                  <p>${desc}</p>
+                  <ul class="blog-post-meta">
+                    <li><i class="fa fa-clock-o"></i> ${date}</li>
+                  </ul>
+                </div>
+              </article>
+            </div>
+          `;
+
+          document.querySelector('#articles-list').innerHTML += html;
+
+          if (index === 0) {
+            document.querySelector('#trending-posts').innerHTML = html;
+          }
+        });
     });
   });
