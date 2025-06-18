ba6808e6d1c010eaa0ffeaaadf2129275c93012c
KhurandaaboldTuguldur <hitugii.tb@gmail.com>
2025-06-18 13:22:33 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,25 +1,20 @@
-fetch('/posts/index.json')
+$(document).ready(function(){"use strict";document.getElementById("copyrightYear").innerHTML=(new Date).getFullYear(),$(".widget-slider").slick({dots:!1,infinite:!0,speed:300,slidesToShow:1,slidesToScroll:1,arrows:!0,autoplay:!0,responsive:[{breakpoint:992,settings:{slidesToShow:1,slidesToScroll:1}},{breakpoint:768,settings:{slidesToShow:1,slidesToScroll:1}}]}),$(window).on("scroll",function(){$(window).scrollTop()?$("nav").addClass("nav-bg"):$("nav").removeClass("nav-bg")})});
+fetch('/posts/index.json') // JSON файлд бүх markdown жагсаалт байна
   .then(res => res.json())
-  .then(items => {
-    items.forEach((item, index) => {
+  .then(posts => {
+    const articleList = document.getElementById('articles-list');
+    const trendingList = document.getElementById('trending-posts');
+    posts.forEach((post, i) => {
       const html = `
-        <div class="col-lg-4">
-          <article class="blog-post">
-            <div class="blog-post-thumb">
-              <img src="${item.thumbnail}" alt="thumb" />
-            </div>
-            <div class="blog-post-content">
-              <a href="single-blog.html" class="blog-post-title">${item.title}</a>
-              <p>${item.description}</p>
-              <ul class="blog-post-meta">
-                <li><i class="fa fa-clock-o"></i> ${item.date}</li>
-              </ul>
-            </div>
-          </article>
-        </div>`;
-      document.querySelector('#articles-list').innerHTML += html;
-      if (index === 0) {
-        document.querySelector('#trending-posts').innerHTML = html;
-      }
+        <div class="col-lg-4 blog-post">
+          <a href="single-blog.html?slug=${post.slug}">
+            <img src="${post.thumbnail}" alt="">
+            <h4>${post.title}</h4>
+            <p>${post.excerpt}</p>
+          </a>
+        </div>
+      `;
+      articleList.innerHTML += html;
+      if (i < 3) trendingList.innerHTML += html;
     });
   });
