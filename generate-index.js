9b850ed77ff1ad484ec3fbf72c90bbc66ad683fa
KhurandaaboldTuguldur <hitugii.tb@gmail.com>
2025-06-24 14:46:00 +0900

    2
    2

--- generate-index.js
+++ generate-index.js
@@ -11,6 +11,7 @@ fs.readdirSync(postsDir).forEach((file) => {
   const ext = path.extname(file);
   const fullPath = path.join(postsDir, file);
 
+  // index.json 등 md가 아닌 파일은 무시
   if (ext === ".md" && fs.lstatSync(fullPath).isFile()) {
     const slug = path.basename(file, ".md");
     const fileContent = fs.readFileSync(fullPath, "utf-8");
@@ -25,11 +26,11 @@ fs.readdirSync(postsDir).forEach((file) => {
         slug: slug
       });
     } else {
-      console.warn(`⚠️ "${file}" файлд title, description, эсвэл date дутуу байна`);
+      console.warn(`⚠️ "${file}" 파일에 title, description, date 중 하나 이상이 없습니다.`);
     }
   }
 });
 
 allPosts.sort((a, b) => new Date(b.date) - new Date(a.date));
 fs.writeFileSync(outputFile, JSON.stringify(allPosts, null, 2));
-console.log(`✅ ${allPosts.length} нийтлэл index.json файлд бичигдлээ`);
+console.log(`✅ ${allPosts.length}개의 포스트가 index.json에 저장되었습니다.`);
