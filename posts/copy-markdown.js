0a34b01693c82c56c8de56117650b1d7b441b348
KhurandaaboldTuguldur <hitugii.tb@gmail.com>
2025-06-27 09:41:39 +0900

    2
    2

--- posts/copy-markdown.js
+++ posts/copy-markdown.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const srcDir = path.join(__dirname, 'posts');
-const destDir = path.join(__dirname, 'dist', 'posts'); // ← 이 줄이 꼭 필요해요!!
+const destDir = path.join(__dirname, 'dist', 'posts'); // ← ❗ 이 줄을 꼭 넣어야 함!
 
 fs.mkdirSync(destDir, { recursive: true });
 
